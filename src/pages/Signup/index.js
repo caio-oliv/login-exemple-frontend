f622231582b9d46b57d453f87521b4dc0fcb118e
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-04-19 09:36:14 -0300

    feat: add Signup page

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,13 +1,56 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
+import auth from '../../services/auth';
 
 import Footer from '../../components/Footer';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Form, InputGroup } from './styles';
 
 function SignUp() {
+	const history = useHistory();
+
+	async function handleSubmit(formData) {
+		try {
+			const response = await api.post('users', formData);
+
+			const { user, token } = response.data;
+
+			auth.login(token);
+			history.push(`/user/${user.id}`);
+
+		} catch (err) {
+			const status = err.response?.status;
+
+			if (status === 400)
+				alert('Um usuário com essas credenciais já existe');
+			else
+				alert('Ocorreu um erro durante o cadastro, tente novamente');
+		}
+	}
+
 	return (
-		<div>
-			<div>Signup</div>
+		<Container>
+			<div>
+				<Form onSubmit={handleSubmit}>
+					<h1>Cadastrar</h1>
+
+					<Input type="email" name="email" placeholder="E-mail" />
+					<Input type="text" name="username" placeholder="Usuário" />
+					<InputGroup>
+						<Input type="text" name="firstName" placeholder="Primeiro nome" />
+						<Input type="text" name="lastName" placeholder="Último nome" />
+					</InputGroup>
+					<Input type="password" name="password" placeholder="Senha" />
+
+					<Button type="submit">Criar conta</Button>
+					<Link to="/">Tenho uma conta</Link>
+				</Form>
+			</div>
 			<Footer />
-		</div>
+		</Container>
 	);
 }
 
---
+++ src/pages/Signup/styles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import { Form as Unform } from '@unform/web';
+
+export const Container = styled.div`
+	height: 100vh;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-between;
+
+	> div {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		height: 100%;
+	}
+`;
+
+export const Form = styled(Unform)`
+  display: flex;
+	flex-direction: column;
+	justify-items: center;
+
+	padding: 20px 16px;
+	border: solid 2px ${({ theme }) => theme.colors.border};
+	border-radius: 12px;
+
+	h1 {
+		text-align: center;
+
+		margin-bottom: 18px;
+
+		font-weight: normal;
+		font-size: 2.3em;
+	}
+
+	a {
+		text-align: center;
+		text-decoration: none;
+
+		margin-top: 32px;
+
+		color: ${({ theme }) => theme.colors.secondary};
+	}
+`;
+
+export const InputGroup = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+
+	div {
+		width: 49%;
+	}
+`;
